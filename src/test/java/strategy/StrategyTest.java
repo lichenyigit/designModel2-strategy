a98d4aa8aede175468080e08cad5b716dadd4d36
lichenyigit <lichenyiwin@outlook.com>
2017-06-30 17:15:36 +0800

    完善测试类

--- src/test/java/strategy/StrategyTest.java
+++ src/test/java/strategy/StrategyTest.java
@@ -1,7 +1,6 @@
 package test.java.strategy;
 
-import main.java.strategy.AddCalculate;
-import main.java.strategy.Context;
+import main.java.strategy.*;
 
 /**
  * @author Lichenyi
@@ -13,7 +12,13 @@ public class StrategyTest {
         int paramA = 4;
         int paramB = 2;
         Context context = new Context(new AddCalculate(paramA, paramB));
-        System.out.println(context.getResult());
+        System.out.println("加法 "+context.getResult());
+        Context contextSub = new Context(new SubtractCalculate(paramA, paramB));
+        System.out.println("减法 "+contextSub.getResult());
+        Context contextMulti = new Context(new MultiplicationCalculate(paramA, paramB));
+        System.out.println("乘法 "+contextMulti.getResult());
+        Context contextDev = new Context(new DevisionCalculate(paramA, paramB));
+        System.out.println("除法 "+contextDev.getResult());
     }
 
 }
